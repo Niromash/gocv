cc81f696478478f4fac638ec310c8471d4c26b50
Mattéo <bl4ise.chr@gmail.com>
2024-09-25 10:59:13 +0200

    Fix cgo in contrib & cuda

--- contrib/cgo.go
+++ contrib/cgo.go
@@ -6,9 +6,9 @@ package contrib
 // Changes here should be mirrored in gocv/cgo.go and cuda/cgo.go
 
 /*
-#cgo !windows pkg-config: opencv4
+#cgo !windows LDFLAGS: -L/usr/local/lib
 #cgo CXXFLAGS:   --std=c++11
-#cgo windows  CPPFLAGS:   -IC:/opencv/build/install/include
-#cgo windows  LDFLAGS:    -LC:/opencv/build/install/x64/mingw/lib -lopencv_core4100 -lopencv_face4100 -lopencv_videoio4100 -lopencv_imgproc4100 -lopencv_highgui4100 -lopencv_imgcodecs4100 -lopencv_objdetect4100 -lopencv_features2d4100 -lopencv_video4100 -lopencv_dnn4100 -lopencv_xfeatures2d4100 -lopencv_plot4100 -lopencv_tracking4100 -lopencv_img_hash4100 -lopencv_calib3d4100 -lopencv_bgsegm4100 -lopencv_xphoto4100 -lopencv_aruco4100 -lopencv_wechat_qrcode4100 -lopencv_ximgproc4100
+#cgo windows  LDFLAGS:   -LC:/opencv/cvglue/bin
+#cgo LDFLAGS: -lgocv_contrib
 */
 import "C"
